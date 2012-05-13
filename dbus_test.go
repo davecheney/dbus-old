0f1c74a3a63d551bd7ea0a6d28fc942ba71aecd3
Dave Cheney <dave@cheney.net>
2012-05-13 11:04:24 +1000

    Added TODO about hard coded session address

--- dbus_test.go
+++ dbus_test.go
@@ -2,9 +2,10 @@ package dbus
 
 import (
 	"testing"
+	"os"
 )
 
-func TestDial(t *testing.T) {
+func TestDialSystemBus(t *testing.T) {
 	c, err := DialUnix("/var/run/dbus/system_bus_socket")
 	if err != nil {
 		t.Fatal(err)
@@ -14,3 +15,16 @@ func TestDial(t *testing.T) {
 	}
 	c.Close()
 }	
+
+func TestDialSessionBus(t *testing.T) {
+	// TODO(dfc) hard coded, need to decode DBUS_SESSION_BUS_ADDRESS
+	c, err := DialUnix("@/tmp/dbus-dsfqbFM8Bp")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := c.Auth(); err != nil {
+		t.Fatal(err)
+	}
+	c.Close()
+}	
+
